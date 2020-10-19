7041d8e5588beb6bebec0d2935dadf157bc41dbb
Mikeyouf <mik_anarchy@hotmail.com>
2020-10-19 20:52:57 +0200

    correction bug script vendor bis

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,19 +1,16 @@
 const React = require("react")
 
-exports.onRenderBody = ({
-    setHeadComponents
-}, pluginOptions) => {
-    setHeadComponents([ <
-        meta
-        name = "viewport"
-        content = "width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
-        key = "1" /
-        >
-        ,
-        // <script
-        //   key="2"
-        //   type="text/javascript"
-        //   src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js"
-        // />,
-    ])
+exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  setHeadComponents([
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
+      key="1"
+    />,
+    // <script
+    //   key="2"
+    //   type="text/javascript"
+    //   src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js"
+    // />,
+  ])
 }
